96235a1fd117fc45cf315c0829f46570adba3ee4
Mike Bertram <contact@mibexx.de>
2019-04-05 11:18:34 +0200

    Changes

--- syn2local.sh
+++ syn2local.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-rsync -av moot@10.10.11.1:~/spryker-academy/ ./
+if [ "$1" == "try" ]; then
+    echo "Try sync"
+    rsync -aun --stats --progress --delete --exclude=".idea/" --exclude=".git/" --exclude=".*" moot@10.10.11.1:~/spryker-academy/ ./
+else
+    echo "Real sync"
+    rsync -au --stats --progress --delete --exclude=".idea/" --exclude=".git/" --exclude=".*" moot@10.10.11.1:~/spryker-academy/ ./
+fi
