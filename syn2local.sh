b515f5a19a18227ee4c8e93205e29e1d471c6f81
Mike Bertram <contact@mibexx.de>
2019-04-05 11:19:00 +0200

    Delete syn2local.sh

--- syn2local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "try" ]; then
-    echo "Try sync"
-    rsync -aun --stats --progress --delete --exclude=".idea/" --exclude=".git/" --exclude=".*" moot@10.10.11.1:~/spryker-academy/ ./
-else
-    echo "Real sync"
-    rsync -au --stats --progress --delete --exclude=".idea/" --exclude=".git/" --exclude=".*" moot@10.10.11.1:~/spryker-academy/ ./
-fi
